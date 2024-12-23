85b1e2b308b82b96df7f7bbd088ca7bfdad24d4d
pablooliveira77 <pablolimadeoliveira77@gmail.com>
2024-12-23 17:28:36 -0300

    disabled

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, onClick, isValid }: IButtonProps) => {
+  return (
+    <ButtonContainer
+      onClick={onClick}
+      disabled={!isValid}
+    >
+      {title}
+    </ButtonContainer>
+  );
+};
+
+export default Button;
